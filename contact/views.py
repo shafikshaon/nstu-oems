d26eb5d1ca6f66f9850bdea0a1b583b9195d9f97
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-06-24 00:12:18 +0600

    contact page implemented

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.views import View
+
+
+class ContactView(View):
+    def get(self, request):
+        return render(request, 'contact/contact.html')
